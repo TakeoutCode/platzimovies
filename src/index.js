a6f3a02eb3414530f147ef110af9e48a52d7cc06
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-17 16:11:48 -0500

    feat(root): Add Layout for App

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { App } from "@routes/App";
+import { Layout } from "@pages/Layout";
+
+import "@styles/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <h1>hola</h1>
+    <Layout>
+      <App />
+    </Layout>
   </React.StrictMode>
 );
