b749d309155331b5162a272613731d0b5c98b691
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-17 13:30:49 -0500

    feat(root): Add styles to footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+export const Footer = () => {
+  return (
+    <div className={styles.footer}>
+      <h2>Hecho con amor por @takeout</h2>
+    </div>
+  );
+};
