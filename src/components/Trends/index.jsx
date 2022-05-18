d2f346227ff7d9ba0b687700305df5335d1cbc5e
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-18 17:50:14 -0500

    feat{root): Add layout for Trends

---
+++ src/components/Trends/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Carousel } from "@components/Carousel";
+import { Categories } from "@components/Categories";
+
+import styles from "./styles.module.scss";
+
+export const Trends = () => {
+  return (
+    <div className={styles.trends}>
+      <Carousel button title="Trends" />
+      <Categories />
+    </div>
+  );
+};
