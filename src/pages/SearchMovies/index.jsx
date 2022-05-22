fc919895d5b33f42a6fc25d635808df45742dccc
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-21 19:15:51 -0500

    refactor: Add feature separation for movie list

--- src/pages/SearchMovies/index.jsx
+++ src/pages/SearchMovies/index.jsx
@@ -1,39 +1,26 @@
 import React from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { MdOutlineArrowBackIosNew } from "react-icons/md";
 import { Search } from "@components/Search";
 import { Button } from "@elements/Button";
+import { ListMovies } from "@components/ListMovies";
+import { Category } from "@components/Categories/Category";
 
 import styles from "./styles.module.scss";
 
 export const SearchMovies = () => {
+  const navigate = useNavigate();
+  const { name, id } = useParams();
   return (
-    <div className={styles.searchMovies}>
-      <MdOutlineArrowBackIosNew className={styles.arrow} />
+    <div className={`${styles.searchMovies} category__colorDegradGenre--${id}`}>
+      <MdOutlineArrowBackIosNew
+        className={styles.arrow}
+        onClick={() => navigate("/")}
+      />
+      {name && id ? <Category title={name} id={id} active /> : <Search />}
 
-      <Search />
+      <ListMovies />
 
-      <figure className={styles.listMovie}>
-        <img
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          className={styles.listMovie_image}
-          alt=""
-        />
-        <img
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          className={styles.listMovie_image}
-          alt=""
-        />
-        <img
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          className={styles.listMovie_image}
-          alt=""
-        />
-        <img
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          className={styles.listMovie_image}
-          alt=""
-        />
-      </figure>
       <div className={styles.showmore}>
         <Button large>Show more</Button>
       </div>
