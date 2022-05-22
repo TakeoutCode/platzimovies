bd54077fabc0b55fc1b662b3aa05691e37df1451
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-21 19:14:38 -0500

    feat: Add category component and styles

---
+++ src/components/Categories/Category/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./styles.module.scss";
+
+export const Category = ({ title, id, active }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className={styles.category}
+      onClick={() => navigate(`/category/${title.trim()}/${id}`)}
+    >
+      <div
+        className={`${styles.category_icon} category__colorGenre--${id}`}
+      ></div>
+      <p id={active ? styles.title : null} className={styles.category_title}>
+        {title}
+      </p>
+    </div>
+  );
+};
