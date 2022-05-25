5e648a2eaccfa09f0cdf6e07609f4161cd4abf13
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 16:50:16 -0500

    feat(root): Add useGetCategories and  useNavigate

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,19 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Button } from "@elements/Button";
-import { useGetMovies } from "@hooks/useGetData";
+import { useGetData } from "@hooks/useGetData";
 import { Movie } from "@components/movie";
 
 import styles from "./styles.module.scss";
+import { Navigate } from "react-router-dom";
 
 export const Carousel = ({ button, title }) => {
+  const navigate = useNavigate();
   const URL = `trending/movie/day`;
-  const [dataMovies] = useGetMovies(URL);
-  console.log(dataMovies);
+  const { dataMovies } = useGetData({ url: URL, category: false });
   return (
     <>
       <div className={styles.container}>
         <h2 className={styles.container_title}>{title}</h2>
-        {button ? <Button>Show more</Button> : null}
+        {button ? (
+          <Button onClick={() => navigate(`trends/day`)}>Show more</Button>
+        ) : null}
       </div>
       <figure className={styles.carousel}>
         {dataMovies.map((trend) => (
