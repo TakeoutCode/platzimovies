875dc296ca1813704de2ee5a2c5d1c5b45cf3207
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 17:01:14 -0500

    feat(root):  Add custom hook to get the categories

---
+++ src/hooks/useCategories.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export function useCategories({ url }) {
+  const [dataMovies, setDataMovies] = useState([]);
+  useEffect(() => {
+    const fetchData = async (url) => {
+      const api = axios.create({
+        baseURL: "https://api.themoviedb.org/3/",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+        },
+      });
+      const { data } = await api.get(url);
+
+      setDataMovies(data.genres);
+    };
+    if (typeof url !== "undefined" && url !== null) {
+      fetchData(url);
+    }
+  }, []);
+  return { dataMovies };
+}
