a9c91a925f47190765262bb29d90fa096ffe7c5b
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 16:51:43 -0500

    feat(root): Add movie component

---
+++ src/components/movie/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import styles from "./styles.module.scss";
+
+export const Movie = ({ title, poster_path, id }) => {
+  const navigate = useNavigate();
+  return (
+    <img
+      src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
+      className={styles.movie__image}
+      alt={title}
+      onClick={() => navigate(`/movie/${id}`)}
+    />
+  );
+};
