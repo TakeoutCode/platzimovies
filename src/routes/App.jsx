db4c0d15d047099761af87d3795033afffb18c35
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 18:34:33 -0500

    build: Change routes to movie-app

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,15 +8,20 @@ import { SearchMovies } from "@pages/SearchMovies";
 import { SearchCategories } from "@pages/SearchCategories";
 
 export const App = () => {
-  const url = 'https://takeout85.github.io/movie-app/'
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="https://takeout85.github.io/movie-app/" element={<Home />} />
-        <Route path="https://takeout85.github.io/movie-app/movie/:id" element={<Movie />} />
-        <Route path="https://takeout85.github.io/movie-app/search/movies/:search" element={<SearchMovies />} />
-        <Route path="https://takeout85.github.io/movie-app/category/:name/:id" element={<SearchCategories />} />
-        <Route path="https://takeout85.github.io/movie-app/trends/:date" element={<SearchCategories />} />
+        <Route path="movie-app/" element={<Home />} />
+        <Route path="movie-app/movie/:id" element={<Movie />} />
+        <Route
+          path="movie-app/search/movies/:search"
+          element={<SearchMovies />}
+        />
+        <Route
+          path="movie-app/category/:name/:id"
+          element={<SearchCategories />}
+        />
+        <Route path="movie-app/trends/:date" element={<SearchCategories />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
