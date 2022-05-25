9d812b753cd4429ddf81502faa99f993edd9d20e
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 18:13:40 -0500

    feat(root): Remove routes

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,13 +11,11 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="https://takeout85.github.io/movie-app/">
-          <Route index element={<Home />} />
-          <Route path="movie/:id" element={<Movie />} />
-          <Route path="search/movies/:search" element={<SearchMovies />} />
-          <Route path="category/:name/:id" element={<SearchCategories />} />
-          <Route path="trends/:date" element={<SearchCategories />} />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path="movie/:id" element={<Movie />} />
+        <Route path="search/movies/:search" element={<SearchMovies />} />
+        <Route path="category/:name/:id" element={<SearchCategories />} />
+        <Route path="trends/:date" element={<SearchCategories />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
