74b200230da6897fe76f6806ce1475a7b9a24b6f
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 16:48:44 -0500

    feat(root): Add useGetCategories and  category modularization

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,47 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import { useCategories } from "@hooks/useCategories";
+import { Category } from "@components/Categories/Category";
 
 import styles from "./styles.module.scss";
 
-const categoriesDefault = [
-  {
-    category: "Action",
-    color: styles.yellow,
-  },
-  {
-    category: "Romance",
-    color: styles.red,
-  },
-  {
-    category: "Adventure",
-    color: styles.green,
-  },
-  {
-    category: "Musical",
-    color: styles.aqua,
-  },
-  {
-    category: "Comedy",
-    color: styles.lightBlue,
-  },
-  {
-    category: "Drama",
-    color: styles.darkBlue,
-  },
-];
+export const Categories = ({ genres }) => {
+  const URL = `genre/movie/list?language=en-US`;
 
-export const Categories = ({ categoriesMovie }) => {
-  let initialState = categoriesMovie || categoriesDefault;
+  const { dataMovies } = useCategories({
+    url: genres ? null : URL,
+  });
+  const dataGenres = genres || dataMovies;
   return (
     <>
       <h3 className={styles.title}>Categories</h3>
       <div className={styles.categories}>
-        {initialState.map((category) => (
-          <div className={styles.category} key={category.category}>
-            <div
-              className={`${category.color} ${styles.category_colorIcon}`}
-            ></div>
-            <p className={styles.category_title}>{category.category}</p>
-          </div>
+        {dataGenres.map((category) => (
+          <Category key={category.id} title={category.name} id={category.id} />
         ))}
       </div>
     </>
