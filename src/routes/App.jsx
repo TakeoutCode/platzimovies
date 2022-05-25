bdd608943f66907f2277828a98f1c7cd9495f30f
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 17:55:40 -0500

    feat(root): Add mode production

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,11 +11,13 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="movie/:id" element={<Movie />} />
-        <Route path="search/movies/:search" element={<SearchMovies />} />
-        <Route path="category/:name/:id" element={<SearchCategories />} />
-        <Route path="trends/:date" element={<SearchCategories />} />
+        <Route path="https://takeout85.github.io/movie-app/">
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search/movies/:search" element={<SearchMovies />} />
+          <Route path="category/:name/:id" element={<SearchCategories />} />
+          <Route path="trends/:date" element={<SearchCategories />} />
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/",
+    publicPath: "./",
   },
   devtool: "source-map",
   mode: "development",
