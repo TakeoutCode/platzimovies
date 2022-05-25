a650657ee51f6df435c5a77daefd4452e1ef8d93
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-25 17:13:16 -0500

    feat(root): Add  custom hook to search movies

---
+++ src/hooks/useSearch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+export function useSearch() {
+  const [searchParams, setSearchParams] = useState("");
+  const [page, setPage] = useState(1);
+  const [movies, setMovies] = useState([]);
+  const { search } = useParams();
+  const URL = `search/movie?language=en-US&query=${search}&page=${page}&include_adult=false`;
+  useEffect(() => {
+    const fetchData = async (url) => {
+      const api = axios.create({
+        baseURL: "https://api.themoviedb.org/3/",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        params: {
+          api_key: process.env.REACT_APP_API_KEY,
+        },
+      });
+
+      const { data } = await api.get(url);
+      if (search === searchParams && page > 1) {
+        setMovies([...movies, ...data.results]);
+      } else {
+        setMovies(data.results);
+        setPage(1);
+      }
+      setSearchParams(search);
+    };
+    if (typeof search !== "undefined" && search.length >= 2) {
+      if (search !== searchParams) {
+        fetchData(URL);
+      } else if (page > 1) {
+        fetchData(URL);
+      }
+    }
+  }, [page, search]);
+
+  return { page, setPage, movies };
+}
