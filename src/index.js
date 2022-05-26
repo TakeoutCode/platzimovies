8b7862b3d377eddb017e3007a2a490966c2ce6f8
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-26 12:26:12 -0500

    feat(root): Add  Helmet async package and configuration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { App } from "@routes/App";
 import { Layout } from "@pages/Layout";
 
@@ -7,8 +8,10 @@ import "@styles/styles.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
+    <HelmetProvider>
+      <Layout>
+        <App />
+      </Layout>
+    </HelmetProvider>
   </React.StrictMode>
 );
