ba8b0d4500b1f01e1852ecaf811d78cafbf364e4
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-30 17:02:07 -0500

    feat: Add CardReviewSkeleton and CardReview components

--- src/pages/Movie/index.jsx
+++ src/pages/Movie/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { MdOutlineArrowBackIosNew } from "react-icons/md";
-import { AiFillStar } from "react-icons/ai";
+import { useParams } from "react-router-dom";
 import { useGetData } from "@hooks/useGetData";
 import { Categories } from "@components/Categories";
 import { Carousel } from "@components/Carousel";
 import { HelmetLayout } from "@components/HelmetLayout";
 import { ArrowBack } from "@components/ArrowBack";
+import { CardReview } from "@components/CardReview";
+import { CardReviewSkeleton } from "@components/Skeletons/CardReviewSkeleton";
 
 import styles from "./styles.module.scss";
 
@@ -14,26 +14,21 @@ export const Movie = () => {
   window.scrollTo(0, 0);
   const { id } = useParams();
   const URL = `movie/${id}`;
-  const { dataMovies } = useGetData({ url: URL, dataType: "movie" });
+  const { dataMovies, loading } = useGetData({ url: URL, dataType: "movie" });
+
+  const backgroundImage = loading
+    ? { background: "#2a0646" }
+    : {
+        backgroundImage: `url(https://image.tmdb.org/t/p/original/${dataMovies.backdrop_path})`,
+      };
   return (
     <div className={styles.movie}>
       <HelmetLayout title={dataMovies.title} subtitle={dataMovies.overview} />
       <ArrowBack />
-      <div
-        style={{
-          backgroundImage: `url(https://image.tmdb.org/t/p/original/${dataMovies.backdrop_path})`,
-        }}
-        className={styles.movie_image}
-      />
+      <div style={backgroundImage} className={styles.movie_image} />
       <div className={styles.movie_review}>
-        <div className={styles.container}>
-          <h1 className={styles.movie_title}>{dataMovies.title}</h1>
-          <div className={styles.movie_qualification}>
-            <AiFillStar className={styles.movie_star} />
-            <p className={styles.movie_value}>{dataMovies.vote_average}</p>
-          </div>
-        </div>
-        <p className={styles.movie_resume}>{dataMovies.overview}</p>
+        {loading ? <CardReviewSkeleton /> : <CardReview {...dataMovies} />}
+
         <Categories genres={dataMovies.genres} />
         <Carousel title="Similar movies" URL={`/movie/${id}/recommendations`} />
       </div>
