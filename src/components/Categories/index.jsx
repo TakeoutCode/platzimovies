de32d4251b51e8acc851f7dabac475e4ae98928b
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-30 16:58:10 -0500

    feat: Add skeleton for categories

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useCategories } from "@hooks/useCategories";
 import { Category } from "@components/Categories/Category";
+import { CategoriesSkeleton } from "@components/Skeletons/CategorySkeleton";
 
 import styles from "./styles.module.scss";
 
 export const Categories = ({ genres }) => {
   const URL = `genre/movie/list?language=en-US`;
 
-  const { dataMovies } = useCategories({
+  const { dataMovies, loading } = useCategories({
     url: genres ? null : URL,
   });
   const dataGenres = genres || dataMovies;
@@ -15,9 +16,17 @@ export const Categories = ({ genres }) => {
     <>
       <h3 className={styles.title}>Categories</h3>
       <div className={styles.categories}>
-        {dataGenres.map((category) => (
-          <Category key={category.id} title={category.name} id={category.id} />
-        ))}
+        {loading ? (
+          <CategoriesSkeleton />
+        ) : (
+          dataGenres.map((category) => (
+            <Category
+              key={category.id}
+              title={category.name}
+              id={category.id}
+            />
+          ))
+        )}
       </div>
     </>
   );
