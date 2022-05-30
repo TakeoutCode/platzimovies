3e9b9728c6962994e87c0a162c724fc0c9cb6b03
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-30 17:00:51 -0500

    feat: Add  categorySkeleton component

---
+++ src/components/Skeletons/CategorySkeleton/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./styles.module.scss";
+
+export const CategoriesSkeleton = () => {
+  return (
+    <>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+      <div className={styles.category}></div>
+    </>
+  );
+};
