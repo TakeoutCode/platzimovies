9619eaaf943e3a49b048c708732082cb2f7338fe
Braulio Quezada <t4k30ut85@gmail.com>
2022-05-31 19:41:36 -0500

    feat: Add lazy loading

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 
 export const Movie = ({ title, poster_path, id, list }) => {
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
+  const element = useRef();
+
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0];
+      if (isIntersecting) {
+        setShow(true);
+        observer.disconnect();
+      }
+    });
+    observer.observe(element.current);
+  }, [element]);
   return (
     <img
-      src={`https://image.tmdb.org/t/p/w200/${poster_path}`}
       id={list && styles.movie__image_list}
       className={styles.movie__image}
+      src={show ? `https://image.tmdb.org/t/p/w500/${poster_path}` : ""}
       loading="lazy"
       alt={title}
+      ref={element}
       onClick={() => navigate(`/movie-app/movie/${id}`)}
     />
   );
