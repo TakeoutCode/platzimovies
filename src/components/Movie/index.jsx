222b9c521480952665eedea2735d3e3715178151
Braulio Quezada <t4k30ut85@gmail.com>
2022-06-01 11:22:31 -0500

    feat: Add default image for movies

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -19,14 +19,27 @@ export const Movie = ({ title, poster_path, id, list }) => {
     observer.observe(element.current);
   }, [element]);
   return (
-    <img
-      id={list && styles.movie__image_list}
-      className={styles.movie__image}
-      src={show ? `https://image.tmdb.org/t/p/w500/${poster_path}` : ""}
-      loading="lazy"
-      alt={title}
-      ref={element}
-      onClick={() => navigate(`/movie-app/movie/${id}`)}
-    />
+    <>
+      {poster_path ? (
+        <img
+          id={list && styles.movie__image_list}
+          className={styles.movie__image}
+          src={show ? `https://image.tmdb.org/t/p/w500/${poster_path}` : ""}
+          loading="lazy"
+          alt={title}
+          ref={element}
+          onClick={() => navigate(`/movie-app/movie/${id}`)}
+        />
+      ) : (
+        <div
+          id={list && styles.movie__image_list}
+          className={styles.movie__image}
+          ref={element}
+          onClick={() => navigate(`/movie-app/movie/${id}`)}
+        >
+          <p>{title}</p>
+        </div>
+      )}
+    </>
   );
 };
