1e672fb5636b253ab32c6e8fe447e0cfa1e3225d
Braulio Quezada <t4k30ut85@gmail.com>
2022-06-11 14:14:35 -0500

    feat: Add features when there is a session

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@elements/Button";
 import { useGetData } from "@hooks/useGetData";
 import { Movie } from "@components/Movie";
 import { CardMovieSkeleton } from "@components/Skeletons/CardMovieSkeleton";
+import UserContext from "@context/index.jsx";
 
 import styles from "./styles.module.scss";
 
-export const Carousel = ({ button, title, URL }) => {
+export const Carousel = ({ title, URL }) => {
   const navigate = useNavigate();
+  const { user } = useContext(UserContext);
 
-  const { dataMovies, loading } = useGetData(URL);
+  const { dataMovies, loading } = useGetData(URL, user.session_id);
+
+  const handleClick = () => {
+    console.log(session_id);
+    navigate(`/movie-app/${title.replace(/ /g, "")}`);
+  };
+  const classFigure = user.success ? "" : styles.session;
   return (
     <>
       <div className={styles.container}>
         <h2 className={styles.container_title}>{title}</h2>
-        {button ? (
-          <Button onClick={() => navigate(`/movie-app/trends/day`)}>
-            Show more
-          </Button>
-        ) : null}
+        {dataMovies.length >= 20 && (
+          <Button onClick={handleClick}>Show more</Button>
+        )}
       </div>
-      <figure className={styles.carousel}>
+      <figure id={classFigure} className={styles.carousel}>
         {loading ? (
           <CardMovieSkeleton />
         ) : (
-          dataMovies.map((trend) => <Movie key={trend.id} {...trend} />)
+          dataMovies.map((item) => (
+            <Movie key={item.id} list={user.success ? false : true} {...item} />
+          ))
         )}
       </figure>
     </>
