01833ad2d7cc6a5f4cb0befa095607805a988f4e
Braulio Quezada <t4k30ut85@gmail.com>
2022-06-11 14:13:18 -0500

    feat: Add like feature

--- src/components/CardReview/index.jsx
+++ src/components/CardReview/index.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { Liked } from "@components/CardReview/Liked";
 
 import styles from "./styles.module.scss";
 
-export const CardReview = ({ title, vote_average, overview }) => {
+export const CardReview = ({ title, vote_average, overview, id }) => {
   return (
     <>
-      <div className={styles.cardReview}>
-        <h1 className={styles.cardReview__title}>{title}</h1>
+      <h1 className={styles.cardReview__title}>{title}</h1>
+      <div className={styles.cardReview__media}>
         <div className={styles.cardReview__qualification}>
           <AiFillStar className={styles.cardReview__star} />
           <p className={styles.cardReview__value}>{vote_average}</p>
         </div>
+        <Liked id={id} />
       </div>
       <p className={styles.cardReview__resume}>{overview}</p>
     </>
