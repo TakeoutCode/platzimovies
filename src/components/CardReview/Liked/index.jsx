e5ccb7613bf3fb5efdab5d274e1d4b1652c39326
Braulio Quezada <t4k30ut85@gmail.com>
2022-06-11 20:22:08 -0500

    feat: Add initial state for movies

--- src/components/CardReview/Liked/index.jsx
+++ src/components/CardReview/Liked/index.jsx
@@ -1,44 +1,33 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { BsHeartFill } from "react-icons/bs";
 import api from "@utils/axiosPreset";
 import UserContext from "@context/index.jsx";
 
 import styles from "./styles.module.scss";
 
-export const Liked = ({ id }) => {
+export const Liked = ({ id, like, setLike }) => {
   const { details, user } = useContext(UserContext);
-  const [like, setLike] = useState(false);
-  const classLiked = like ? styles.liked : "";
-  useEffect(() => {
-    const movieLiked = async () => {
-      const { data } = await api.get(
-        `/movie/${id}/account_states?session_id=${user.session_id}`
-      );
-      setLike(data.favorite);
-    };
-
-    if (user.success) {
-      movieLiked();
-    }
-  }, []);
+  const idLiked = like ? styles.liked : "";
 
   const liked = async () => {
-    const { data } = await api.post(
-      `/account/${details.id}/favorite?session_id=${user.session_id}`,
-      {
-        media_type: "movie",
-        media_id: id,
-        favorite: !like,
+    if (user.success) {
+      const { data } = await api.post(
+        `/account/${details.id}/favorite?session_id=${user.session_id}`,
+        {
+          media_type: "movie",
+          media_id: id,
+          favorite: !like,
+        }
+      );
+      if (data.status_code === 1 || data.status_code === 13) {
+        setLike(!like);
       }
-    );
-    if (data.status_code === 1 || data.status_code === 13) {
-      setLike(!like);
     }
   };
 
   return (
     <BsHeartFill
-      id={classLiked}
+      id={idLiked}
       className={styles.cardReview__heart}
       onClick={liked}
     />
